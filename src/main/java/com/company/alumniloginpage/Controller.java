fc8fdf1164e5b9cda9439335a9a21c2e6cc531d3
arhn3487 <arhasan3487@gmail.com>
2025-02-08 13:00:54 +0600

    update alumniCard

--- src/main/java/com/company/alumniloginpage/Controller.java
+++ src/main/java/com/company/alumniloginpage/Controller.java
@@ -198,6 +198,15 @@ public class Controller {
         stage.show();
     }
 
+    public void switchalumniCard(ActionEvent event) throws IOException {
+        //Parent root = loadFXML(load.(getClass().getResource("SignUp.fxml")));
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("alumniCard.fxml"))));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void switchToCreateAccount(ActionEvent event) throws IOException {
         //Parent root = loadFXML(load.(getClass().getResource("SignUp.fxml")));
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("CreateAccount.fxml"))));
