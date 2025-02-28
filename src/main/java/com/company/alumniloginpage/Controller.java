7bf229aca92c71f8e649fd059c55d3a5aeeead44
arhn3487 <arhasan3487@gmail.com>
2025-03-01 00:33:40 +0600

    id card added

--- src/main/java/com/company/alumniloginpage/Controller.java
+++ src/main/java/com/company/alumniloginpage/Controller.java
@@ -511,8 +511,26 @@ public class Controller
         stage.setFullScreen(true);
         stage.show();
     }
+
+    public void switchtoStudentIdCard(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("student_id_card.fxml"))));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
     public void switchToJobBoard(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("job.fxml"))));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    public void switchtoAlumniListStudent(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("alumniListStudent.fxml"))));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
